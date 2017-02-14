102f335f680f1fb680ee76ff8c04cdd82e06736c
Nathan <nazzanuk@gmail.com>
2017-02-14 19:33:39 +0000

    :)

--- release/static/app.js
+++ release/static/app.js
@@ -167,21 +167,6 @@ app.component('tabsComponent', {
         return tabsComponent;
     }()
 });
-app.controller('VotingScreen', function () {
-    function VotingScreen($element, $timeout, $interval, $scope, $rootScope, $compile) {
-        _classCallCheck(this, VotingScreen);
-    }
-
-    _createClass(VotingScreen, [{
-        key: '$onInit',
-        value: function $onInit() {
-            console.log(this);
-        }
-    }]);
-
-    return VotingScreen;
-}());
-
 app.controller('HomeScreen', function () {
     function HomeScreen($element, $timeout, $interval, $scope, $rootScope, $compile) {
         _classCallCheck(this, HomeScreen);
@@ -195,4 +180,19 @@ app.controller('HomeScreen', function () {
     }]);
 
     return HomeScreen;
+}());
+
+app.controller('VotingScreen', function () {
+    function VotingScreen($element, $timeout, $interval, $scope, $rootScope, $compile) {
+        _classCallCheck(this, VotingScreen);
+    }
+
+    _createClass(VotingScreen, [{
+        key: '$onInit',
+        value: function $onInit() {
+            console.log(this);
+        }
+    }]);
+
+    return VotingScreen;
 }());
