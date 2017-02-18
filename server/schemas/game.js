2778f5e11b960ac0f9c2db4681c034f4f400ca89
Nathan <nazzanuk@gmail.com>
2017-02-18 06:38:41 +0000

    :)

--- server/schemas/game.js
+++ server/schemas/game.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const GUID = require('../services/guid');
+const Cards = require('../services/cards');
 
 const schema = mongoose.Schema({
     _id: {type: String, required: true, default: GUID.generate},
     status: {type: String, default: 'new'},
     host: {type: String, required: true},
     players: [],
-    cards: [],
+    cards: {type:[], default: Cards.generate},
     rounds: [],
     messages: [],
     currentRound: {type: Number, default: 0},
---
+++ server/services/cards.js
@@ -0,0 +1,43 @@
+const _ = require('lodash');
+
+
+const Game = require('../schemas/game');
+
+const stdErr = err => res.status(400).json(err);
+
+const cards = [
+    'would live to be 120 years old and still achieve nothing',
+    'would happily listen to one song for the rest of their life',
+    `think's they are a big deal`,
+    `is incredibly stupid, but super lucky`,
+    `plays too much video games`,
+    `eats too much broccoli`,
+    `drinks too much coffee`,
+    `reads too much self-help`,
+    `makes too much money`,
+    `thinks they are a big deal`,
+    `loves themselves way more than they should`,
+    `is probably very good in bed`,
+    `probably has some odd fetishes`,
+    `looks better with glasses`,
+    `needs to see a fashion guru`,
+    `could live in a bathroom for the rest of their life`,
+    `would glue their eyes shut with super glue`,
+    `is most likely to actuallly be a vampire`,
+    `is habouring negative feelings`,
+    `could do with a telling off`,
+    `has horrible music taste`,
+    `is trying way too hard`,
+    `would eat a worm straight from the ground`,
+    `would make a good predator`,
+    `would make a good rabbit`,
+    `should be sent to a distant planet`,
+    `would you want as your backup if shit went down`,
+    `is probably thinking about something else right now`
+]
+
+module.exports = {
+    generate(socket, data) {
+        return _.sampleSize(cards, 10);
+    }
+};
